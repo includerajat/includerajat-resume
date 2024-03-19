4d53be316e937b5f12c50a35ec737ecbc4b53454
Rajat Jindal <jindalrajat80@gmail.com>
2024-03-19 14:49:07 +0530

    Add functionality to add the custom resume

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
 const name = document.querySelector(".name");
 const company = document.querySelector(".company");
+const preview = document.querySelector(".resume-preview");
+const download = document.querySelector(".resume-download");
 
 const queryString = window.location.search;
 const urlParams = new URLSearchParams(queryString);
 
 const nameValue = urlParams.get("name");
 const companyValue = urlParams.get("company");
+
+const previewUrl = `https://drive.google.com/file/d/${
+  urlParams.get("id") ?? "1llEd-rSzqsP9J52q-P-GgqPkIm8cD4YO"
+}/preview`;
+const downloadUrl = `https://drive.google.com/uc?export=download&id=${
+  urlParams.get("id") ?? "1llEd-rSzqsP9J52q-P-GgqPkIm8cD4YO"
+}`;
 
 if (nameValue) {
   name.innerHTML = nameValue;
@@ -14,3 +23,6 @@ if (nameValue) {
 if (companyValue) {
   company.innerHTML = companyValue;
 }
+
+preview.src = previewUrl;
+download.href = downloadUrl;
