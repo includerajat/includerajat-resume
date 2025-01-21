6261c03994e36336574fa4a686f13d3d2560713b
Rajat Jindal <78809406+includerajat@users.noreply.github.com>
2025-01-21 09:49:58 +0530

    Add functionality to share resume with HM
    Use "referral" query paramter as false

--- app.js
+++ app.js
@@ -2,19 +2,19 @@ const name = document.querySelector(".name");
 const company = document.querySelector(".company");
 const preview = document.querySelector(".resume-preview");
 const download = document.querySelector(".resume-download");
+const referralLine = document.querySelector(".referral .line");
 
 const queryString = window.location.search;
 const urlParams = new URLSearchParams(queryString);
 
 const nameValue = urlParams.get("name");
 const companyValue = urlParams.get("company");
+const isReferral = urlParams.get("referral");
 
-const previewUrl = `https://drive.google.com/file/d/${
-  urlParams.get("id") ?? "1llEd-rSzqsP9J52q-P-GgqPkIm8cD4YO"
-}/preview`;
-const downloadUrl = `https://drive.google.com/uc?export=download&id=${
-  urlParams.get("id") ?? "1llEd-rSzqsP9J52q-P-GgqPkIm8cD4YO"
-}`;
+const previewUrl = `https://drive.google.com/file/d/${urlParams.get("id") ?? "1llEd-rSzqsP9J52q-P-GgqPkIm8cD4YO"
+  }/preview`;
+const downloadUrl = `https://drive.google.com/uc?export=download&id=${urlParams.get("id") ?? "1llEd-rSzqsP9J52q-P-GgqPkIm8cD4YO"
+  }`;
 
 if (nameValue) {
   name.innerHTML = nameValue;
@@ -22,6 +22,12 @@ if (nameValue) {
 
 if (companyValue) {
   company.innerHTML = companyValue;
+}
+
+console.log(referralLine);
+
+if (isReferral === "false") {
+  referralLine.innerHTML = "Thank you for considering me for the open position in";
 }
 
 preview.src = previewUrl;
